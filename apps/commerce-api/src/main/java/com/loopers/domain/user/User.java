500534547b690d1ff6255b35badb13d289f4a2d6
hainho <hainho9704@gmail.com>
2025-07-15 15:32:46 +0900

    feat: 유저 도메인 구현

--- apps/commerce-api/src/main/java/com/loopers/domain/user/User.java
+++ apps/commerce-api/src/main/java/com/loopers/domain/user/User.java
@@ -1,4 +1,38 @@
 package com.loopers.domain.user;
 
-public class User {
+import com.loopers.domain.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseEntity {
+    @Embedded
+    private UserId userId;
+    @Enumerated(value = EnumType.STRING)
+    private Gender gender;
+    @Embedded
+    private Email email;
+    @Embedded
+    private BirthDate birthDate;
+
+    private User(UserId userId, Gender gender, Email email, BirthDate birthDate) {
+        this.userId = userId;
+        this.gender = gender;
+        this.email = email;
+        this.birthDate = birthDate;
+    }
+
+    public static User register(UserCommand.UserRegisterCommand command) {
+        return new User(
+                new UserId(command.userId()),
+                Gender.from(command.gender()),
+                new Email(command.email()),
+                new BirthDate(command.birthDate())
+        );
+    }
 }
