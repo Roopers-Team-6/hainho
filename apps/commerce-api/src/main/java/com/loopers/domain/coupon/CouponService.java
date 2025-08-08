5c19b11a19e563f64566aa59db75a8576d091555
hainho <hainho9704@gmail.com>
2025-08-08 16:52:59 +0900

    feat: 쿠폰 사용 서비스 구현

---
+++ apps/commerce-api/src/main/java/com/loopers/domain/coupon/CouponService.java
@@ -0,0 +1,36 @@
+package com.loopers.domain.coupon;
+
+import jakarta.persistence.EntityNotFoundException;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+public class CouponService {
+    private final CouponIssuanceRepository couponIssuanceRepository;
+    private final CouponRepository couponRepository;
+
+    @Transactional
+    public Long useCoupon(Long couponIssuanceId, Long orderId, Long orderPrice) {
+        CouponIssuance couponIssuance = getCouponIssuance(couponIssuanceId);
+
+        Long couponId = couponIssuance.getCouponId();
+        Coupon coupon = getCoupon(couponId);
+        Long discountAmount = coupon.calculateDiscountAmount(orderPrice);
+
+        couponIssuance.use(orderId);
+        return discountAmount;
+    }
+
+    private CouponIssuance getCouponIssuance(Long couponIssuanceId) {
+        return couponIssuanceRepository.findById(couponIssuanceId)
+                .orElseThrow(() -> new EntityNotFoundException("쿠폰 발급 정보를 찾을 수 없습니다. 쿠폰 발급 ID: " + couponIssuanceId));
+    }
+
+    private Coupon getCoupon(Long couponId) {
+        return couponRepository.findById(couponId)
+                .orElseThrow(() -> new EntityNotFoundException("쿠폰 정보를 찾을 수 없습니다. 쿠폰 ID: " + couponId));
+    }
+}
