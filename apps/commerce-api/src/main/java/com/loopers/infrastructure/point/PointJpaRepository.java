d58ca399a84407f9fe07ac8e45d194c2109b581d
hainho <hainho9704@gmail.com>
2025-08-08 17:11:47 +0900

    feat: 최소 품질 체크 리스트에 따라 비관적락 timeout 5초로 설정

--- apps/commerce-api/src/main/java/com/loopers/infrastructure/point/PointJpaRepository.java
+++ apps/commerce-api/src/main/java/com/loopers/infrastructure/point/PointJpaRepository.java
@@ -2,9 +2,11 @@ package com.loopers.infrastructure.point;
 
 import com.loopers.domain.point.Point;
 import jakarta.persistence.LockModeType;
+import jakarta.persistence.QueryHint;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
 
 import java.util.Optional;
 
@@ -13,6 +15,7 @@ public interface PointJpaRepository extends JpaRepository<Point, Long> {
     Optional<Point> findByUserId(Long userId);
 
     @Lock(value = LockModeType.PESSIMISTIC_WRITE)
+    @QueryHints(@QueryHint(name = "javax.persistence.lock.timeout", value = "5000"))
     @Query("SELECT p FROM Point p WHERE p.userId = :userId")
     Optional<Point> findByUserIdWithLock(Long userId);
 
