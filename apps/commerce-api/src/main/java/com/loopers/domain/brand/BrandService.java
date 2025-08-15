74b6f8dccd277439ca49b0ebf3cab8441ab18b7f
hainho <hainho9704@gmail.com>
2025-08-15 17:52:04 +0900

    feat: 브랜드 정보 캐시

--- apps/commerce-api/src/main/java/com/loopers/domain/brand/BrandService.java
+++ apps/commerce-api/src/main/java/com/loopers/domain/brand/BrandService.java
@@ -9,15 +9,19 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 public class BrandService {
+    private final BrandRedisRepository brandRedisRepository;
     private final BrandRepository brandRepository;
 
-    public BrandInfo.Get getBrandInfo(Long id) {
-        Brand brand = getBrand(id);
+    public BrandInfo.Get getBrandInfo(Long brandId) {
+        Brand brand = brandRedisRepository.find(brandId)
+                .orElse(getAndCacheBrand(brandId));
         return BrandInfo.Get.from(brand);
     }
 
-    private Brand getBrand(Long id) {
-        return brandRepository.find(id)
-                .orElseThrow(() -> new CoreException(ErrorType.NOT_FOUND, "id에 해당하는 브랜드를 찾을 수 없습니다. brandId: " + id));
+    private Brand getAndCacheBrand(Long brandId) {
+        Brand brand = brandRepository.find(brandId)
+                .orElseThrow(() -> new CoreException(ErrorType.NOT_FOUND, "id에 해당하는 브랜드를 찾을 수 없습니다. brandId: " + brandId));
+        brandRedisRepository.set(brandId, brand);
+        return brand;
     }
 }
